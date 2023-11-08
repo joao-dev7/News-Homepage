24dc1b8d2d3c511d732c94c04b130536db5251e2
João Pedro Silva <joaopedrosilvasalesss@gmail.com>
2023-11-08 19:31:49 -0300

    Menu e dependentes

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const conteiner = document.querySelector(".conteiner")
+const nav = document.querySelector("nav")
+
+const menuIcon = document.createElement("img")
+menuIcon.classList.add("nav-menu")
+menuIcon.setAttribute('src', '../assets/images/icon-menu.svg')
+
+const menuBar = document.createElement("div")
+menuBar.classList.add("conteiner-menuBar")
+
+const menuClose = document.createElement("img")
+menuClose.classList.add("conteiner-menuBar-menuClose")
+menuClose.setAttribute('src', '../assets/images/icon-menu-close.svg')
+
+if (conteiner.style.height <= "983px") {
+    nav.appendChild(menuIcon)
+    
+    menuIcon.onclick = () => {
+        conteiner.appendChild(menuBar)
+        conteiner.appendChild(menuClose)
+        menuClose.onclick = () => {
+            conteiner.removeChild(menuBar)
+            conteiner.removeChild(menuClose)
+        }
+    }
+}
